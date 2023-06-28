cebf2da09345a2583953dff2ea865c5f5c4301e7
saroj <sarojtmg992@gmail.com>
2023-06-28 21:21:29 +0100

    finished

--- logic.js
+++ logic.js
@@ -1,42 +1,14 @@
-// const gameBoard = {
-//     gameBoard: [],
+const player = (function () {
+    return {
+        icon: function (icon) {
+            return { icon }
+        }
+    }
+}
+)()
 
-//     players: function (icon) {
-//         return {
-//             icon
-//         }
-//     }
-// }
-
-// const one = gameBoard.players('x')
-// const two = gameBoard.players('O')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const one = player.icon('X')
+const two = player.icon('O')
 
 const grids = document.querySelectorAll('.grid')
 const winnerText = document.querySelector('.result')
@@ -47,25 +19,26 @@ let win = true;
 let xIndex = [];
 let oIndex = [];
 
+const newX = []
+const newO = []
 
-
-
-
-
-
-
-
-
-
-
-
+const winLogic = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
 grids.forEach((grids, index) => {
     grids.addEventListener('click', () => {
         count++;
 
         if (count % 2 === 0) {
-            grids.textContent = 'O';
+            grids.textContent = two.icon
             grids.style.fontSize = '90px'
             grids.style.pointerEvents = 'none' // stops the dive from chaning the player.
             oIndex.push(index)
@@ -75,7 +48,7 @@ grids.forEach((grids, index) => {
                 oIndex = []
             }
         } else {
-            grids.textContent = 'X'
+            grids.textContent = one.icon
             grids.style.fontSize = '90px'
             grids.style.pointerEvents = 'none'
             xIndex.push(index)
@@ -90,44 +63,58 @@ grids.forEach((grids, index) => {
         const xSort = xIndex.sort((a, b) => a - b)
         const oSort = oIndex.sort((a, b) => a - b)
 
-        if (xSort[0] === 0 && xSort[1] === 1 && xSort[2] === 2
+
+        for (let i = 0; i < winLogic.length; i++) {
+            const element = winLogic[i];
+
+            const xFilter = xSort.filter(arr => element.includes(arr))
+            const oFilter = oSort.filter(arr => element.includes(arr))
+
+            if (xFilter.length === 3) {
+                newX.push(xFilter[0], xFilter[1], xFilter[2])
+            } else if (oFilter.length === 3) {
+                newO.push(oFilter[0], oFilter[1], oFilter[2])
+            }
+
+        }
+
+        if (newX[0] === 0 && newX[1] === 1 && newX[2] === 2
             ||
-            xSort[0] === 3 && xSort[1] === 4 && xSort[2] === 5
+            newX[0] === 3 && newX[1] === 4 && newX[2] === 5
             ||
-            xSort[0] === 6 && xSort[1] === 7 && xSort[2] === 8
+            newX[0] === 6 && newX[1] === 7 && newX[2] === 8
             ||
-            xSort[0] === 0 && xSort[1] === 3 && xSort[2] === 6
+            newX[0] === 0 && newX[1] === 3 && newX[2] === 6
             ||
-            xSort[0] === 1 && xSort[1] === 4 && xSort[2] === 7
+            newX[0] === 1 && newX[1] === 4 && newX[2] === 7
             ||
-            xSort[0] === 2 && xSort[1] === 5 && xSort[2] === 8
+            newX[0] === 2 && newX[1] === 5 && newX[2] === 8
             ||
-            xSort[0] === 0 && xSort[1] === 4 && xSort[2] === 8
+            newX[0] === 0 && newX[1] === 4 && newX[2] === 8
             ||
-            xSort[0] === 2 && xSort[1] === 4 && xSort[2] === 6
+            newX[0] === 2 && newX[1] === 4 && newX[2] === 6
         ) {
             winnerText.textContent = 'X Won'
         }
 
-        else if (oSort[0] === 0 && oSort[1] === 1 && oSort[2] === 2
+        else if (newO[0] === 0 && newO[1] === 1 && newO[2] === 2
             ||
-            oSort[0] === 3 && oSort[1] === 4 && oSort[2] === 5
+            newO[0] === 3 && newO[1] === 4 && newO[2] === 5
             ||
-            oSort[0] === 6 && oSort[1] === 7 && oSort[2] === 8
+            newO[0] === 6 && newO[1] === 7 && newO[2] === 8
             ||
-            oSort[0] === 0 && oSort[1] === 3 && oSort[2] === 6
+            newO[0] === 0 && newO[1] === 3 && newO[2] === 6
             ||
-            oSort[0] === 1 && oSort[1] === 4 && oSort[2] === 7
+            newO[0] === 1 && newO[1] === 4 && newO[2] === 7
             ||
-            oSort[0] === 2 && oSort[1] === 5 && oSort[2] === 8
+            newO[0] === 2 && newO[1] === 5 && newO[2] === 8
             ||
-            oSort[0] === 0 && oSort[1] === 4 && oSort[2] === 8
+            newO[0] === 0 && newO[1] === 4 && newO[2] === 8
             ||
-            oSort[0] === 2 && oSort[1] === 4 && oSort[2] === 6
+            newO[0] === 2 && newO[1] === 4 && newO[2] === 6
         ) {
             winnerText.textContent = 'O Won'
         }
-
 
         else if (xSort.length === 5 || oSort.length === 5) {
             console.log('draw');
@@ -135,265 +122,3 @@ grids.forEach((grids, index) => {
 
     })
 })
-
-
-/* Disable the game and create a message with the result of win and draw  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Come back here after you have created the logic of the game flow */
-
-
-// const x = document.querySelector('#x')
-
-// const o = document.querySelector('#o')
-
-// x.addEventListener('click', (e) => {
-//     const xIcon = document.createElement('p')
-//     xIcon.textContent = 'X'
-//     console.log(xIcon);
-//     //no need for if statement - this runs first.
-// })
-
-// o.addEventListener('click', () => {
-//     const oIcon = document.createElement('p')
-//     oIcon.textContent = 'O'
-//     console.log(oIcon);
-//     // You might need to create a if statement when the player chooses O.
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Restart button */
